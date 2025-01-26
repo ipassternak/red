7b3cdd3d20089f6ee6ea1081066c84ab38e93595
Ivan Pasternak <retriumpf@gmail.com>
2025-01-26 13:58:29 +0200

    feat: add trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { setupGracefulShutdown } from 'nestjs-graceful-shutdown';
 import { Logger } from 'nestjs-pino';
 
@@ -8,11 +9,14 @@ import { AppConfig } from '@lib/types/config';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
   setupGracefulShutdown({ app });
   const logger = app.get(Logger);
   app.useLogger(logger);
   const config = app.get(ConfigService<AppConfig, true>);
+  app.set('trust proxy', config.get('server.trustProxy', { infer: true }));
   await app.listen(config.get('server.port', { infer: true }));
 }
 
